9a835d57c37e51d6e516038d890890a49fc5c243
Chandra Prakash <157478950+kumaryadavcimage@users.noreply.github.com>
2025-11-30 10:20:49 +0000

    Number topic

--- basic1/numAndString.js
+++ basic1/numAndString.js
@@ -1,4 +1,4 @@
-const firstName="Hello"
+/*const firstName="Hello"
 const lastName='Hitesh Choudhory'
 console.log(`My firstName ${firstName} is and my lastName is ${lastName}`)
 
@@ -14,4 +14,19 @@ let emailTwo="hc@gmail.com"
 console.log(emailTwo.replace('hc','hitesh'))
 console.log(email.includes('c'))
 // method all runs as parenthecis ()
-console.log(emailTwo.toUpperCase())
+console.log(emailTwo.toUpperCase()) */
+
+const num = new Number(5.2345)
+console.log(num.toFixed(2));
+
+const subsribers = 100000
+console.log(subsribers.toLocaleString())
+console.log(typeof(subsribers.toLocaleString))
+console.log(Number.MAX_VALUE)
+
+console.log(Math)
+console.log(Math.random())
+console.log(Math.floor(Math.random()))
+console.log(Math.floor(Math.random()*10));
+console.log(Math.floor(Math.random() + 1 ))
+console.log(Math.floor(Math.random()) + 1)
