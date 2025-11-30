0e88d9f8329cc60880785100faa05ffbc4d50f20
Chandra Prakash <157478950+kumaryadavcimage@users.noreply.github.com>
2025-11-30 12:01:08 +0000

    array1

---
+++ basic1/array.js
@@ -0,0 +1,27 @@
+const myArray=[1,2,3,4,5]
+const myNewArray = new Array(1,2,3,4,5)
+
+const dcHeros = ["batman","superman","flash","GL","Arrow"]
+// console.log(dcHeros.length)
+
+dcHeros.push("wonder woman")
+console.log(dcHeros);
+// const lastHero = dcHeros.pop()
+// console.log(dcHeros)
+// dcHeros.unshift('aquaman')
+// console.log(dcHeros);
+// dcHeros.shift('aaa') this is not move 
+
+// console.log(dcHeros);
+// console.log(myArray.toString())
+// console.log(dcHeros.toString())
+// console.log(dcHeros.toLocaleString());
+
+// console.log(dcHeros.join("_"));
+console.log(dcHeros.slice(1,3));
+console.log(dcHeros)
+console.log(dcHeros.splice(1,3))
+console.log(dcHeros)
+
+
+
